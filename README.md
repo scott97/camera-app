2284515184e6929089ec715596923aaa499151d2
Scott <scottbarnard5@gmail.com>
2019-01-02 20:38:38 +1100

    Added nunjucks

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ An infra-red triggered wildlife camera app for the raspberry pi.
 
 
 access using http://192.168.1.108/
+
+to run program use
+```
+sudo node app.js
+```
--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => res.send('Hello World!'))
+nunjucks.configure('views', {
+    autoescape: true,
+    express   : app
+  });
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res) {
+    res.render('index.html', {
+      title : 'My First Nunjucks Page',
+      items : [
+        { name : 'item #1' },
+        { name : 'item #2' },
+        { name : 'item #3' },
+        { name : 'item #4' },
+      ]
+    });
+  });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
