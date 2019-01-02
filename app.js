6d200b9ac6455efe47f8418b0610cee97dea1c4d
Scott <scottbarnard5@gmail.com>
2019-01-02 21:02:33 +1100

    Made public folder accessible

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const path = require('path');
 const app = express()
 const port = 80
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 nunjucks.configure('views', {
     autoescape: true,
