a77af94555f21dc10f150a971b2ace932db3b6b5
Scott <scottbarnard5@gmail.com>
2019-01-02 21:12:06 +1100

    Added basic app ui

--- app.js
+++ app.js
@@ -9,14 +9,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 nunjucks.configure('views', {
     autoescape: true,
-    express   : app
-  });
+    express: app
+});
 
-// app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index.njk', {
-      title : 'Raspberry Pi',
+        title: 'Raspberry Pi',
     });
-  });
+});
+
+app.get('/download-pictures', function (req, res) {
+    res.render('index.njk', {
+        title: 'Raspberry Pi',
+    });
+});
+
+
 
 app.listen(port, () => console.log(`Http request to port ${port}.`))
