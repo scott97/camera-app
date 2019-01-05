7a628046ffbea6fced13808fa499a83f96be1fd0
Scott <scottbarnard5@gmail.com>
2019-01-05 19:28:54 +1100

    added picamera

--- app.js
+++ app.js
@@ -4,4 +4,20 @@ const port = 80
 // Start Server for controlling pi
 server.listen(port, () => console.log(`Server started on port ${port}.`))
 
-// Do rpi stuff
+// Do camera stuff
+const PiCamera = require('pi-camera');
+const myCamera = new PiCamera({
+  mode: 'photo',
+  output: `${ __dirname }/test.jpg`,
+  width: 640,
+  height: 480,
+  nopreview: false,
+});
+ 
+myCamera.snap()
+  .then((result) => {
+    // Your picture was captured
+  })
+  .catch((error) => {
+     // Handle your error
+  });
