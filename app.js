f13e5f3c373c5a203543ac354ee95904f2647a01
Scott <scottbarnard5@gmail.com>
2019-01-05 21:22:24 +1100

    changed output file location of picamera

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ server.listen(port, () => console.log(`Server started on port ${port}.`))
 const PiCamera = require('pi-camera');
 const myCamera = new PiCamera({
   mode: 'photo',
-  output: `${ __dirname }/test.jpg`,
+  output: '~./test.jpg',
   width: 640,
   height: 480,
   nopreview: false,
