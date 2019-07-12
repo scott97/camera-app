9277f4d328873590fd02c94e4c0e1566a2a4e2a4
scott97 <scottbarnard5@gmail.com>
2019-07-12 20:13:56 +1000

    fix - removed import of pages router

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const root = '/home/pi/Camera-App'
 const express = require('express')
 const apiRouter = require(root + '/server/routers/api.js')
 const imagesRouter = require(root + '/server/routers/images.js')
-const pagesRouter = require(root + '/server/routers/pages.js')
 
 // Exports
 module.exports = express()
